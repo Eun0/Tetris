89990e6d637cf2e437eddbccbd2c54482e609a3f
Eun0 <33515697+Eun0@users.noreply.github.com>
2018-08-03 19:21:52 +0900

    Fix kicking wall #3

--- NoClass/firstShape.py
+++ NoClass/firstShape.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Shape:
 
 	def __init__(self,num,leftTop):
@@ -45,7 +48,8 @@ class Shape:
 						   
 		#위치
 		self.leftTop=leftTop
-		self.potLeftTop=[self.leftTop[0],self.leftTop[1]]
+		self.pot_leftTop=[self.leftTop[0],self.leftTop[1]]
+		self.pot_shape=copy.deepcopy(self.shape)
 
 
 
@@ -55,64 +59,65 @@ class Shape:
 		# 정사각형 
 		if self.num==1:
 
-			self.shape=[[1,1]
+			self.pot_shape=[[1,1]
 					   ,[1,1]]
 		# 'L' 			   
 		elif self.num==2:
 
 			if self.rotation%3==0:
-				self.shape=[[1,1]
+				self.pot_shape=[[1,1]
 				           ,[0,1]
 				           ,[0,1]]
 
 			elif self.rotation%3==1:
-				self.shape=[[0,0,1]
+				self.pot_shape=[[0,0,1]
 						   ,[1,1,1]]
 
 			elif self.rotation%3==2:
-				self.shape=[[1,0]
+				self.pot_shape=[[1,0]
 						   ,[1,0]
 						   ,[1,1]]
 		# 'l'
 		elif self.num==3:
 
 			if self.rotation%2==0:
-				self.shape=[[1]
+				self.pot_shape=[[1]
 						   ,[1]
 						   ,[1]
 						   ,[1]]
 
 			elif self.rotation%2==1:
-				self.shape=[[1,1,1,1]]
+				self.pot_shape=[[1,1,1,1]]
 
 		# 'Z' 			   
 		elif self.num==4:
 
 			if self.rotation%2==0:
-				self.shape=[[1,1,0]
+				self.pot_shape=[[1,1,0]
 				           ,[0,1,1]]
 
 			elif self.rotation%2==1:
-				self.shape=[[0,1]
+				self.pot_shape=[[0,1]
 				           ,[1,1]
 				           ,[1,0]]
 
 		# 'ㅗ' 			   
 		elif self.num==5:
 			if self.rotation%4==0:
-				self.shape=[[0,1,0]
+				self.pot_shape=[[0,1,0]
 				           ,[1,1,1]]
 			elif self.rotation%4==1:
-				self.shape=[[1,0]
+				self.pot_shape=[[1,0]
 						   ,[1,1]
 						   ,[1,0]]
 			elif self.rotation%4==2:
-				self.shape=[[1,1,1]
+				self.pot_shape=[[1,1,1]
 						   ,[0,1,0]]
 			elif self.rotation%4==3:
-				self.shape=[[0,1]
+				self.pot_shape=[[0,1]
 				           ,[1,1]
 				           ,[0,1]]
+		return self.pot_shape			           
 
 
 	
--- NoClass/firstTetris.py
+++ NoClass/firstTetris.py
@@ -1,17 +1,20 @@
 import pygame
 import firstShape
 import random
+import copy
 
-# 경계 있는 사각형 그리기 
+
+# 경계 있는 정사각형 그리기 (block)
 def draw(x,y):
 	solid_color=(70,70,70)
-	# outside 크기 20*20 
+	# outside.png 크기 20*20 
 	solid=pygame.image.load("outside.png").convert()
 	solid.fill(solid_color,(1,1,18,18))
 	screen.blit(solid,(x,y))
 
 
 # 가로 줄 체크
+# 다 찼으면 그 줄 지운다
 def line_check(board):
 
 	for row in range(len(board)):
@@ -19,51 +22,56 @@ def line_check(board):
 		for col in range(len(board[row])):
 			checker+=board[row][col]
 			
-			# 칸이 채워져있으면 1이다. 한 줄 다 채워져 있다 col 개수
+			# 칸이 채워져있으면 1이다. 
+			# 한 줄 다 채워져 있으면
+			# => checker == cols
 			if checker==len(board[0]):
 				
+				# 그 줄 없애고
 				board.pop(row)
+
+				# 맨 위에 새로 빈 row 삽입
 				board.insert(0,[0 for col in range(len(board[0]))])
 
 
-
-# (키 눌렀을 때) 움직일 칸 채워져 있는 지 '체크'
-# 채워졌으면 움직일 수 없다. 경계 넘어가는 것도 채워졌다고 하겠다.
-def taken_check(tetromino,board):
+# (키 눌렀을 때) 움직일 칸 채워져 있는 지 '체크' 
+# 채워졌으면 움직일 수 없다(taken=True). taken 리턴 
+# 경계 넘어가는 것도 채워졌다고 하겠다.
+# 움직이기 전 미리 확인하는 것이므로, pot_(potential) 이용
+def taken(tetromino,board):
 
 	taken=False
 
-	for row in range(len(tetromino.shape)):
-		for col in range(len(tetromino.shape[row])):
-			if tetromino.shape[row][col]!=0:
+	for row in range(len(tetromino.pot_shape)):
+		for col in range(len(tetromino.pot_shape[row])):
+			if tetromino.pot_shape[row][col]!=0:
 				# 왼쪽 경계 넘어가는 경우
-				if col+tetromino.potLeftTop[1]<0:
+				if col+tetromino.pot_leftTop[1]<0:
 					taken=True
 					break
 				# 오른쪽 경계 넘어가는 경우
-				elif col+tetromino.potLeftTop[1]>=len(board[0]):
+				elif col+tetromino.pot_leftTop[1]>=len(board[0]):
 					taken=True
 					break
 				# 게임판 이미 채워진 경우
-				elif row+tetromino.potLeftTop[0]>=len(board):
-					taken=True
-					break
-
-				elif board[row+tetromino.potLeftTop[0]][col+tetromino.potLeftTop[1]]:
+				elif board[row+tetromino.pot_leftTop[0]][col+tetromino.pot_leftTop[1]]:
 					taken=True
 					break
 
 	return taken
 
+
 # tet 떨어뜨리기
 # safe가 False면 안착. 땅에 닿은 경우도 safe=False
+# where은 안착할 위치,즉 닿은 곳 세로 어디까지 찼는 지 알려주는 변수 
+# safe,where 리턴
 def falling(tet,board,where):
 
 	safe=True	
 
 	# potential 움직일 위치
-	tet.potLeftTop[0]=tet.leftTop[0]+1
-	tet.potLeftTop[1]=tet.leftTop[1]
+	tet.pot_leftTop[0]=tet.leftTop[0]+1
+	tet.pot_leftTop[1]=tet.leftTop[1]
 	
 	# 땅에 닿았을 때
 	if len(tet.shape)+tet.leftTop[0]>=len(board):
@@ -72,10 +80,10 @@ def falling(tet,board,where):
 		for row in range(len(tet.shape)):
 			for col in range(len(tet.shape[row])):
 				if tet.shape[row][col]!=0:
-					if board[row+tet.potLeftTop[0]][col+tet.potLeftTop[1]]!=0:
+					if board[row+tet.pot_leftTop[0]][col+tet.pot_leftTop[1]]!=0:
 						# 안착 할 위치
 						if where!=0:
-							where=row+tet.potLeftTop[0]-1
+							where=row+tet.pot_leftTop[0]-1
 							
 						safe=False
 
@@ -89,19 +97,26 @@ def draw_tet(tetromino):
 	for row in range(len(tetromino.shape)):
 		for col in range(len(tetromino.shape[row])):
 			if tetromino.shape[row][col]!=0:
+				# block 한 변의 길이 * block 위치 + 시작 위치
 				x=20*(col+tetromino.leftTop[1])+50
 				y=20*(row+tetromino.leftTop[0])+50
+				# block 그리기
 				draw(x,y)
+
 
 # 게임판 그리기
 def draw_board(board):
+	# 경계선 그리기 (시작 위치 (50,50) 으로 함)
 	pygame.draw.rect(screen,color,(50,50,side*len(board[0]),side*len(board)),1)
+
+	# 게임판 채워진 거 그리기
 	for row in range(len(board)):
 		for col in range(len(board[row])):
 			if board[row][col]!=0:
-				x=col*20+50
-				y=row*20+50
+				x=20*col+50
+				y=20*row+50
 				draw(x,y)
+
 
 # tetromino board에 안착하기
 def land(tetromino,board):
@@ -112,7 +127,6 @@ def land(tetromino,board):
 				board[row+tetromino.leftTop[0]][col+tetromino.leftTop[1]]=tetromino.shape[row][col]
 
 																			
-
 
 # 가로
 cols=10
@@ -163,8 +177,6 @@ while not finish:
 	# 게임 진행중
 	if gamestate:
 
-		
-
 		# 게임판 그리기
 		draw_board(board)
 
@@ -185,37 +197,70 @@ while not finish:
 
 			
 		if not collision:
+
 			# 키 눌렀을 때
 			pressed=pygame.key.get_pressed()
 			
-
+			# 윗 방향키 => 회전
 			if pressed[pygame.K_UP]: 
-				shapes[i].rotate()
+
+				# 회전했다고 가정
+				pot_tet=shapes[i].rotate()
+
+				# 회전한 모양이 경계 넘어갈 수도 있음
+				# kick 변수로 확인
+				# kick = 현재 가로 위치+ 회전한 모양의 가로 길이
+				kick=shapes[i].leftTop[1]+len(pot_tet[0])
+
+				# 경계 넘어가면 kick_checker=True
+				kick_checker=False
+
+				# kick이 게임판 가로보다 길다.
+				# => 경계 넘어감
+				if kick>cols:
+
+					kick_checker=True
+
+					# out = 얼만큼 넘어갔는 지
+					out=kick-cols
+
+					# 넘어간 만큼 현재 위치에서 빼준 것을 pot_leftTop에 넣는다.
+					# 객체 생성 시 pot_leftTop leftTop으로 초기화되므로
+					# pot_leftTop에서 out 바로 빼도 된다.
+					shapes[i].pot_leftTop[1]-=out
+
+				# potential 위치에 차지하고 있는 칸이 없다면
+				if not taken(shapes[i],board):
+
+					# 현재 모양 회전한 모양으로 바꾼다.				
+					shapes[i].shape=pot_tet
+					shapes[i].leftTop=copy.deepcopy(shapes[i].pot_leftTop)
+				
 				
 
 			if pressed[pygame.K_DOWN]: 
 				# potential 설정
-				shapes[i].potLeftTop[0]=shapes[i].leftTop[0]+1
-				shapes[i].potLeftTop[1]=shapes[i].leftTop[1]
+				shapes[i].pot_leftTop[0]=shapes[i].leftTop[0]+1
+				shapes[i].pot_leftTop[1]=shapes[i].leftTop[1]
 				# potential 위치에 차지하고 있는 칸이 없다면
-				if not taken_check(shapes[i],board):
-					shapes[i].leftTop[0]+=1
+				if not taken(shapes[i],board):
+					shapes[i].leftTop=copy.deepcopy(shapes[i].pot_leftTop)
 				
 			if pressed[pygame.K_LEFT]:
 				# potential 설정
-				shapes[i].potLeftTop[0]=shapes[i].leftTop[0]
-				shapes[i].potLeftTop[1]=shapes[i].leftTop[1]-1
+				shapes[i].pot_leftTop[0]=shapes[i].leftTop[0]
+				shapes[i].pot_leftTop[1]=shapes[i].leftTop[1]-1
 				# potential 위치에 차지하고 있는 칸이 없다면
-				if not taken_check(shapes[i],board):
-					shapes[i].leftTop[1]-=1
+				if not taken(shapes[i],board):
+					shapes[i].leftTop=copy.deepcopy(shapes[i].pot_leftTop)
 
 			if pressed[pygame.K_RIGHT]:
 				# potential 설정 
-				shapes[i].potLeftTop[0]=shapes[i].leftTop[0]
-				shapes[i].potLeftTop[1]=shapes[i].leftTop[1]+1
+				shapes[i].pot_leftTop[0]=shapes[i].leftTop[0]
+				shapes[i].pot_leftTop[1]=shapes[i].leftTop[1]+1
 				# potential 위치에 차지하고 있는 칸이 없다면
-				if not taken_check(shapes[i],board):
-					shapes[i].leftTop[1]+=1
+				if not taken(shapes[i],board):
+					shapes[i].leftTop=copy.deepcopy(shapes[i].pot_leftTop)
 
 			# 매 루프마다 tetromino 한 칸씩 떨어진다. 
 			# 떨어질 수 없다면 safe=False,where=그 때의 y위치 
@@ -226,7 +271,7 @@ while not finish:
 		# 떨어질 수 있다면 
 		if safe:
 			# potential이 실제 leftTop이 된다.
-			shapes[i].leftTop[0]=shapes[i].potLeftTop[0]
+			shapes[i].leftTop[0]=shapes[i].pot_leftTop[0]
 
 
 		# 떨어질 수 없다면
@@ -267,7 +312,7 @@ while not finish:
 
 
 	pygame.display.flip()
-	clock.tick(6)	
+	clock.tick(7)	
 
 		
 		
